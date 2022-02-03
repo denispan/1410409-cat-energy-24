4239340a3bf20e90b6d6c1d5d236acc386ad815e
denispan <8888670@gmail.com>
2022-02-03 21:42:46 +0300

    Исправил замечания по первой проверке

--- gulpfile.js
+++ gulpfile.js
@@ -89,7 +89,8 @@ const copy = (done) => {
   gulp.src([
     'source/fonts/*.{woff2,woff}',
     'source/*.ico',
-    '*.webmanifest',
+    'source/*.webmanifest',
+    'source/*.ico',
   ], {
     base: 'source'
   })
